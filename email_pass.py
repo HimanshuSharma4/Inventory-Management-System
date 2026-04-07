c29ce76c8e95ad35206857722f0cd125c86e248c
Himanshu Sharma <himanshuharit411@gmail.com>
2026-04-07 18:09:46 +0530

    Secured project with .env and added virtual environment

--- email_pass.py
+++ email_pass.py
@@ -1,2 +1,9 @@
-email_='himanshuharit411@gmail.com'
-pass_='<ENTER_YOUR_APP_PASSWORD_HERE>'
+import os
+from dotenv import load_dotenv
+
+# Ye function chhipi hui .env file ko background mein load kar lega
+load_dotenv()
+
+# Yahan code sidha .env file se tera asli data uthayega
+email_ = os.getenv('MY_EMAIL')
+pass_ = os.getenv('MY_APP_PASSWORD')
